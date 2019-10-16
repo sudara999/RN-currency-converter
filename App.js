bb739aebd27a028afb3b86b6241e9415434db6b1
Sudara Ranasinghe <sudara1999@gmail.com>
2019-10-17 03:02:40 +0900

    App can converts from KRW to USD and vice-versa
    App allows the user to reverse the exchange of currency
    +App contains a 'switch' button to reverse the exchange
    +App only displays up to 2 decimal places for the converted amount

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class AppScreen extends Component {
 
   state = {
-    amount: 0,
+    from:"KRW",
+    to:"USD",
+    amount:'',
     rate: 0
   };
 
-  //Try to get the exchange rate and set rate in this.state.rate
+  //Get the exchange rate and set rate in this.state.rate
   componentDidMount(){
     fetch("https://api.exchangeratesapi.io/latest?base=KRW")
     .then(response => response.json())
@@ -19,7 +21,28 @@ export default class AppScreen extends Component {
     })
   }
 
-  
+  //Switch the two currencies being exchanged from and to
+  switchCurrency(){
+    //from -> to
+    //to -> from
+    this.setState(state => {
+      return{
+        from: state.to, to: state.from, amount: '', rate: 0
+      }
+    },
+    //the new exchange rate for the pair is requesed from the api
+    ()=>{
+      let URL = "https://api.exchangeratesapi.io/latest?base=" + this.state.from;
+      fetch(URL)
+      .then(response => response.json())
+      .then((responseJSON) => {
+        this.setState({
+          rate: responseJSON.rates[this.state.to].toString()
+        });
+      })
+    }
+    );
+  }
 
   render(){
     return(
@@ -27,21 +50,33 @@ export default class AppScreen extends Component {
       <View style={styles.masterContainer}>
         {/*Area to enter from-currency*/}
         <View style={styles.textArea}> 
-          <Text>Enter Amount in KRW</Text>
-          <TextInput
-            style={styles.inputBox} 
-            onChangeText={(text)=>this.setState({amount: text})}
-            keyboardType={"numeric"}
-            placeholder="0"  
-          />
+          <Text>Enter Amount in {this.state.from}</Text>
+          <View style={styles.inputBar}>
+            {/*TextInput to enter from-currency*/}
+            <TextInput
+              style={styles.inputBox} 
+              onChangeText={(text)=>this.setState({amount: text})}
+              keyboardType={"numeric"}
+              placeholder="0"
+              value={String(this.state.amount)}
+              on
+            />
+            {/*Button to reverse the exchange*/}
+            <Button
+              style={styles.button}
+              title="Switch"
+              onPress={()=>this.switchCurrency()}
+              raised
+            />
+          </View>
         </View>
         {/*Area to display to-currency*/}
         <View style={styles.textArea}>
           <Text>
-            Amount in USD    
+            Amount in {this.state.to}    
           </Text>
           <Text>
-            {this.state.amount * this.state.rate}
+            {(this.state.amount * this.state.rate).toFixed(2)}
           </Text>
         </View>
       </View>
@@ -66,5 +101,12 @@ let styles = StyleSheet.create({
     alignItems: "center",
     margin: 5,
     borderRadius: 5,
+  },
+  inputBar: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  button: {
+    borderRadius: 5
   }
 })
